22dccdf26fe862865a284fdf4624b5d7cfe5ae21
Sri Akula <xpaysh@gmail.com>
2025-12-04 16:04:20 +0530

    Patching Nextjs Vulnerability Fix

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -232,7 +232,7 @@ export default async function RootLayout({ children, params }: LayoutProps<'/[la
       {isProd && (
         <>
           <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-5G03XLKQ06"
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
             strategy="afterInteractive"
           />
           <Script id="google-analytics" strategy="afterInteractive">
@@ -240,7 +240,7 @@ export default async function RootLayout({ children, params }: LayoutProps<'/[la
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-5G03XLKQ06');
+                gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
               `}
           </Script>
         </>
